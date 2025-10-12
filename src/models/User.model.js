1537e02c57e2e8de4bd79ab43251de8406a3a85b
samira kibous <kiboussamira71@gmail.com>
2025-10-13 00:10:35 +0100

    export json

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -4,7 +4,8 @@ const userSchema = new mongoose.Schema({
     fullname: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, default: "user", enum: ["user", "admin"] }
+    role: { type: String, default: "user", enum: ["user", "admin"] },
+    deletedAt: { type: Date, default: null } 
 }, {
     timestamps: { createdAt: true, updatedAt: false }
 });
